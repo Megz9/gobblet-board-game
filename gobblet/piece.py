f714ddf4cf70b916a39d11a58c51a17b8ff92028
Abdalla Ibrahim <abdullahibrahim544@gmail.com>
2023-12-16 19:29:40 +0200

    adding functions to check the alignment of the gobblets in all directions)

---
+++ gobblet/piece.py
@@ -0,0 +1,8 @@
+import pygame
+from .constants import *
+
+
+class Piece:
+    def __init__(self, color, size):
+        self.color = color
+        self.size = size
---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+from gobblet.board import Board
+from gobblet.constants import *
+from gobblet.piece import Piece
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Gobblet')
+
+
+def test(board):
+    board.board[0][0].push_piece(Piece(Color.DARK, 1))
+    board.board[0][1].push_piece(Piece(Color.DARK, 2))
+    board.board[0][2].push_piece(Piece(Color.DARK, 3))
+    board.board[0][3].push_piece(Piece(Color.DARK, 4))
+    board.draw_piece(WIN, Piece(Color.DARK, 1), board.board[0][0])
+    board.draw_piece(WIN, Piece(Color.DARK, 2), board.board[0][1])
+    board.draw_piece(WIN, Piece(Color.DARK, 3), board.board[0][2])
+    board.draw_piece(WIN, Piece(Color.DARK, 4), board.board[0][3])
+
+def main():
+    WIN.fill(BACKGROUND)
+    run = True
+    clock = pygame.time.Clock()
+    board = Board(WIN)
+    # test(board)
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.select(WIN)
+                #print(board.to_left_pane,board.to_board,board.to_right_pane)
+
+                # board.move(WIN, board.board[0][3], board.board[0][0])
+        pygame.display.update()
+    pygame.quit()
+
+
+main()
+
+
